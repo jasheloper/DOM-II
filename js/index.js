8bebe3733a9688f71a8373055901689d9634aad1
Jashele T <jashelet@Jashele.domain>
2019-06-07 18:42:17 -0700

    added eventlistener for nav

--- js/index.js
+++ js/index.js
@@ -1,1 +1,35 @@
 // Your code goes here
+
+
+// create 10 unique event listeners. using your creativity to make the Fun Bus site more interactive. 
+
+let aNavLink = document.querySelectorAll('.nav-link');
+const theHeader = document.querySelector('img');
+
+//nav
+aNavLink[0].addEventListener('mouseover', (event) => {
+    event.target.style.color = 'tomato';
+});
+aNavLink[1].addEventListener('mouseover', (event) => {
+    event.target.style.color = 'red';
+});
+aNavLink[2].addEventListener('mouseover', (event) => {
+    event.target.style.color = 'green';
+});
+aNavLink[3].addEventListener('mouseover', (event) => {
+    event.target.style.color = 'blue';
+});
+aNavLink[4].addEventListener('mouseover', (event) => {
+    event.target.style.color = 'orange';
+});
+
+theHeader.addEventListener('dblclick', (event) => {
+ event.classList.toggle('Large'); 
+});
+
+
+
+
+
+
+
