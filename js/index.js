49f1748fa701b6902e3ee0e5348b0e10c6b1e5e6
Jashele T <jashelet@Jashele.domain>
2019-06-07 20:31:11 -0700

    added eventlisteners, total of 4

--- js/index.js
+++ js/index.js
@@ -4,32 +4,91 @@
 // create 10 unique event listeners. using your creativity to make the Fun Bus site more interactive. 
 
 let aNavLink = document.querySelectorAll('.nav-link');
-const theHeader = document.querySelector('img');
+let theP = document.querySelectorAll('p');
+const headerImg = document.querySelector('.intro img');
+const mapImg = document.querySelector('.img-content img');
+
+
+//header
+headerImg.addEventListener('contextmenu', (event) => {
+    alert('I was walking to my car when I noticed my pants were on fire. I turned to my friend and said, I think were going to need an oven and fast.')
+});
+
 
 //nav
 aNavLink[0].addEventListener('mouseover', (event) => {
     event.target.style.color = 'tomato';
 });
-aNavLink[1].addEventListener('mouseover', (event) => {
-    event.target.style.color = 'red';
+aNavLink[0].addEventListener('mouseleave', (event) => {
+    event.target.style.color = 'black';
 });
-aNavLink[2].addEventListener('mouseover', (event) => {
+
+aNavLink[1].addEventListener('mouseover', (event) => {
     event.target.style.color = 'green';
 });
+aNavLink[1].addEventListener('mouseleave', (event) => {
+    event.target.style.color = 'black';
+});
+
+
+aNavLink[2].addEventListener('mouseover', (event) => {
+    event.target.style.color = 'red';
+});
+aNavLink[2].addEventListener('mouseleave', (event) => {
+    event.target.style.color = 'black';
+});
+
 aNavLink[3].addEventListener('mouseover', (event) => {
-    event.target.style.color = 'blue';
+    event.target.style.color = 'hotpink';
 });
-aNavLink[4].addEventListener('mouseover', (event) => {
-    event.target.style.color = 'orange';
-});
-
-theHeader.addEventListener('dblclick', (event) => {
- event.classList.toggle('Large'); 
+aNavLink[3].addEventListener('mouseleave', (event) => {
+    event.target.style.color = 'black';
 });
 
 
 
 
 
+// paragraphs 
+theP[0].addEventListener ('click', (event) => {
+    event.target.style.backgroundColor = 'yellow'; 
+});
 
+theP[1].addEventListener ('copy', (event) => {
+    alert('Text Copied!')
+});
+
+
+// images
+mapImg[0].addEventListener ('mousemove', (event) => {
+  event.target.style.width = '1%';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function addEventToList(list, event, fn) {
+//     for (let q = 0; q < list.length; q++) {
+//       list[q].addEventListener(event, fn);
+//     }
+//    }
+
+//    let trollPar = document.querySelectorAll("p");
+// addEventToList(trollPar, "contextmenu", function() {
+//  this.textContent = "No no";
+//  this.style.color = "crimson";
+// });
 
