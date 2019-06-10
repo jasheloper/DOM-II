e34e01946762411e7505ebbc0fefa15065844dfc
Jashele T <jashelet@Jashele.domain>
2019-06-10 12:52:33 -0700

    set a click event for all images

--- js/index.js
+++ js/index.js
@@ -4,4 +4,12 @@
 // create 10 unique event listeners. using your creativity to make the Fun Bus site more interactive. 
 
 
+const theImages = document.getElementsByTagName('img');
 
+
+theImages[0].addEventListener("click", function(){ alert("Welcome to the Fun Bus!"); });
+theImages[1].addEventListener("click", function(){ alert("Are you ready for an adventure?"); });
+theImages[2].addEventListener("click", function(){ alert("Lots of great activities await you!"); });
+theImages[3].addEventListener("click", function(){ alert("Time to pick your destination!"); });
+
+// console.log(theImages)
